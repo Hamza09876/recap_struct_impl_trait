eaffeb71a44e3ab5e4c58aa05336a7ec2136cc06
Hamza09876 <hamzasajid4040@gmail.com>
2023-03-07 12:32:54 +0500

    added some other points regarding impls

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,24 @@ struct DougsData{
     some_bool: bool,
     random: RandomInfo,
 }
-
+//here is the proof and reason why impl are not part of struct
+//you can extend struct anywhere at any point
+impl RandomInfo {   
+    pub fn is_greater(&mut self, compare_to:i64)-> bool{
+        self.some_int > compare_to
+    }
+}
 #[allow(unused_variables)]
 fn main() {
-    let random_info_var = RandomInfo{
+    let mut random_info_var = RandomInfo{
+        call_count:0,
         some_bool: true,
         some_int: 22,
     };
+
+    //here we use dot operator because self defined is with lowercase s in impl function
     let is_this_smaller = random_info_var.is_smaller(9);
+    let is_this_greater = random_info_var.is_smaller(10);
     //made it mutable so could be changes
     let mut dougs_var = DougsData{
         some_int:80,
@@ -26,6 +36,7 @@ fn main() {
     //here we used double colon to access function because Self is defined on type with capital S
         random: RandomInfo::new(true),
     };
+
     //trying to mutate 
     dougs_var.some_int = 100;
 
--- src/random_info.rs
+++ src/random_info.rs
@@ -1,16 +1,19 @@
 pub struct RandomInfo{
+    pub call_count: i64,
     pub some_bool: bool,
     pub some_int: i64,
 }
 impl RandomInfo {
     pub fn new(param_a: bool) -> Self{
         Self { 
+            call_count: 0,
             some_bool: !param_a, 
             some_int: 8 
         }
     }
 
-    pub fn is_smaller(&self, compare_to:i64)-> bool{
+    pub fn is_smaller(&mut self, compare_to:i64)-> bool{
+        self.call_count += 1;
         self.some_int < compare_to
     }
 }
